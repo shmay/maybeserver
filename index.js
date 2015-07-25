8d162c70a22a80f1c41fdfda6ab39c3c1e32d8b0
Kyle Murphy <kmurph73@gmail.com>
2015-07-25 15:54:47 -0700

    damnit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post('/new_spot', function (req, res) {
       } else {
         var b = req.body;
 
-        var obj = {
+        var spot = {
           name: b.name,
           lat: b.lat,
           lng: b.lng,
@@ -43,12 +43,24 @@ app.post('/new_spot', function (req, res) {
           if (error) {
             res.send({success:0});
           } else {
-            var spotRef = ref.child('spots').push(obj);
+            var spotRef = ref.child('spots').push(spot);
 
             spotRef.child('users/' + b.uid).set(user);
             ref.child('users/' + b.uid + '/spots/' + spotRef.key()).set(true);
 
-            res.send({success:1});
+            spotRef.once('value', function(snap) {
+              var val = snap.val();
+
+              res.send({
+                success:1,
+                id: snap.key(),
+                lat: parseFloat(val.lat),
+                lng: parseFloat(val.lng),
+                name: val.name,
+                radius: parseFloat(val.radius)
+              });
+            });
+
           }
         });
       }
@@ -87,13 +99,43 @@ var generateToken = function(ref,spotid,cb) {
   });
 };
 
-app.post('/gen_invite', function (req, res) {
-  if (req.body.token && req.body.spotid) {
+app.post('/spot_status_changed', function (req, res) {
+  var status = parseInt(req.body.status);
+  console.log('spot_status_changed');
+  console.log(status);
+  console.log(req.body);
+  if (req.body.token && req.body.spotid && (status === 0 || status === 1 || status === 2)) {
+
+    console.log('good');
     ref = new Firebase('https://androidkye.firebaseio.com/');
 
     ref.authWithCustomToken(req.body.token, function(err,authData) { 
-      console.log('authData:');
-      console.log(authData);
+      if (err) {
+        res.send({success:0});
+      } else {
+        ref.authWithCustomToken(process.env.MBSECRET, function(error) {
+          if (error) {
+            res.send({success:0});
+          } else {
+            ref.child('users/' + authData.uid + '/spots/' + req.body.spotid).once('value', function(snap) {
+              var val = snap.val();
+              if (val === null || val === -1) {
+                res.send({success:-1});
+              } else if (val === true || val === false) {
+                ref.child('spots/' + req.body.spotid + '/users/' + authData.uid + '/isthere').set(status);
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+app.post('/gen_invite', function (req, res) {
+  if (req.body.token && req.body.spotid) {
+
+    ref.authWithCustomToken(req.body.token, function(err,authData) { 
       if (err) {
         res.send({success:0});
       } else {
@@ -148,6 +190,184 @@ app.post('/edit_spot', function (req, res) {
   }
 });
 
+app.post('/remove_fence_for_user', function (req, res) {
+  console.log('remove_fence_for_user');
+  if (req.body.token && req.body.spotid) {
+    ref = new Firebase('https://androidkye.firebaseio.com/');
+
+    ref.authWithCustomToken(req.body.token, function(err,authData) { 
+      if (err) {
+        res.send({success:0});
+      } else {
+        ref.authWithCustomToken(process.env.MBSECRET, function(error) {
+          if (error) {
+            res.send({success: 0});
+          } else {
+            var spotRef = ref.child('users/' + authData.uid + '/spots/' + req.body.spotid);
+
+            spotRef.once('value', function(snap) {
+              var val = snap.val();
+
+              if (val === null) {
+                res.send({success: -1});
+              } else {
+                spotRef.remove();
+                res.send({success: 1});
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+app.post('/remove_spot', function (req, res) {
+  if (req.body.token && req.body.spotid) {
+    ref = new Firebase('https://androidkye.firebaseio.com/');
+
+    ref.authWithCustomToken(req.body.token, function(err,authData) { 
+      if (err) {
+        res.send({success:0});
+      } else {
+        ref.authWithCustomToken(process.env.MBSECRET, function(error) {
+          if (error) {
+            res.send({success: 0});
+          } else {
+            var uRef = ref.child('users/' + authData.uid + '/spots/' + req.body.spotid);
+
+            uRef.once('value', function(snap) {
+              var val = snap.val();
+              if (val === true) {
+                res.send({success:-1});
+                ref.child('spots/' + req.body.spotid + '/users').once('value', function(snap) {
+                  var users = snap.val();
+
+                  for (var userid in users) {
+                    ref.child('users/' + userid + '/spots/' + req.body.spotid).set(-1);
+                  }
+
+                  ref.child('spots/' + req.body.spotid).remove();
+                });
+              } else {
+                res.send({success: -1});
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+app.post('/remove_user', function (req, res) {
+  if (req.body.token && req.body.spotid && req.body.uid) {
+    ref = new Firebase('https://androidkye.firebaseio.com/');
+
+    ref.authWithCustomToken(req.body.token, function(err,authData) {
+      if (err) {
+        res.send({success:0});
+      } else {
+        ref.authWithCustomToken(process.env.MBSECRET, function(error) {
+          if (error) {
+            res.send({success: 0});
+          } else {
+            var adminRef = ref.child('users/' + authData.uid + '/spots/' + req.body.spotid);
+            var spotRef = ref.child('users/' + req.body.uid + '/spots/' + req.body.spotid);
+            adminRef.once('value', function(snapshot) {
+              var val = snapshot.val();
+              if (val === true) {
+                spotRef.once('value', function(snap) {
+                  val = snap.val();
+                  if (val === null) {
+                    res.send({success:-1});
+                  } else {
+                    spotRef.set(-1);
+                    ref.child('spots/' + req.body.spotid + '/users/' + req.body.uid).remove();
+                    res.send({success: 1});
+                  }
+                });
+              } else {
+                res.send({success:0});
+              }
+            });
+          }
+        });
+      }
+    });
+  } else {
+    res.send({success:0});
+  }
+});
+
+app.post('/leave_spot', function (req, res) {
+  if (req.body.token && req.body.spotid) {
+    ref = new Firebase('https://androidkye.firebaseio.com/');
+
+    ref.authWithCustomToken(req.body.token, function(err,authData) { 
+      if (err) {
+        res.send({success:0});
+      } else {
+        ref.authWithCustomToken(process.env.MBSECRET, function(error) {
+          if (error) {
+            res.send({success: 0});
+          } else {
+            var spotRef = ref.child('users/' + authData.uid + '/spots/' + req.body.spotid);
+            spotRef.once('value', function(snap) {
+              var val = snap.val();
+              if (val === true || val === false) {
+                spotRef.set(-1);
+                ref.child('spots/' + req.body.spotid + '/users/' + authData).remove();
+                res.send({success: 1});
+              } else {
+                res.send({success: 0});
+              }
+            });
+          }
+        });
+      }
+    });
+  } else {
+    res.send({success:0});
+  }
+});
+
+app.post('/update_name', function (req, res) {
+  console.log('update_name');
+  if (req.body.token && req.body.newname) {
+    ref = new Firebase('https://androidkye.firebaseio.com/');
+
+    ref.authWithCustomToken(req.body.token, function(err,authData) { 
+      if (err) {
+        res.send({success:0});
+      } else {
+        ref.authWithCustomToken(process.env.MBSECRET, function(error) {
+          if (error) {
+            res.send({success: 0});
+          } else {
+            console.log('yup2');
+            var name = req.body.newname;
+            var userRef = ref.child('users/' + authData.uid);
+            userRef.child('name').set(name);
+
+            userRef.child('spots').once('value', function(snap) {
+              var spots = snap.val();
+              for (var spotid in spots) {
+                ref.child('spots/' + spotid + '/users/' + authData.uid + '/name').set(name);
+              }
+
+              res.send({success:1});
+            });
+
+          }
+        });
+      }
+    });
+  } else {
+    res.send({success:0});
+  }
+});
+
 app.post('/join', function (req, res) {
   if (req.body.token && req.body.pin) {
     console.log('PIN');
@@ -169,6 +389,7 @@ app.post('/join', function (req, res) {
             if (val.users[authData.uid]) {
               ref.authWithCustomToken(process.env.MBSECRET, function(error) {
                 if (error) {
+                  res.send({success: 0});
                 } else {
                   uRef.once('value', function(snap) {
                     var name = snap.val().name;
@@ -223,6 +444,7 @@ app.post('/join_w_pin', function (req, res) {
                   res.send({
                     success:1,
                     pin: req.body.pin,
+                    id: spotid,
                     lat: parseFloat(val.lat),
                     lng: parseFloat(val.lng),
                     name: val.name,
